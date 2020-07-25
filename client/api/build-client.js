fdbebfe2f58e0bff8ab71436fa50892c11207da4
Behnam <behnam.ebrahimi@gmail.com>
2020-07-25 19:32:30 +1000

    Update build-client.js

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -8,7 +8,7 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        "http://www.behnam-ticketing-app.xyz",
       headers: req.headers,
     });
   } else {
